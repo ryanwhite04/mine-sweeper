79f0a714186f8c08fbcab156ead348779e915956
ryanwhite04 <ryanwhite04@penguin>
2019-08-12 00:14:25 +0800

    Remove external components

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolvepnp from 'rollup-plugin-pnp-resolve';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+const { dependencies } = require('./package.json');
+const replace = o => ({ name: 'replace', resolveId: i => Object.keys(o).includes(i) ? o[i] : i });
+
+export default [{
+    input: 'modules.js',
+    output: { file: 'dist/modules.js', format: 'esm' },
+    plugins: [
+        // resolvepnp(),
+        resolve()
+    ]
+}, {
+    input: 'index.js',
+    external: Object.keys(dependencies),
+    output: { file: 'dist/cjs.js', format: 'cjs' },
+    plugins: [replace({ "./dist/modules.js": "./modules.js" }), resolve(), commonjs()]
+}, {
+    input: 'index.js',
+    external: Object.keys(dependencies),
+    output: { file: 'dist/esm.js', format: 'esm' },
+    plugins: [replace({ "./dist/modules.js": "./modules.js" }), resolve(), commonjs()]
+}]
